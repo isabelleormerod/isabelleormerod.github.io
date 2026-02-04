5e00f5709b4c3d93174da9d6ea7b94b845348693
isabelleormerod <isabelleormerod@gmail.com>
2026-02-04 15:15:31 +0000

    update header into folder

--- js/main.js
+++ js/main.js
@@ -1,1 +1,10 @@
+function loadHeader() {
+    fetch('header.html')
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById('header-placeholder').innerHTML = data;
+        });
+}
+loadHeader();
+
 console.log("Your website scripts are loaded!");
